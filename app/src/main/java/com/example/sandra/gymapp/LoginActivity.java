9d70e4720b4ae8f2fc5e5e5238046578bc9f93ad
47419119l <47419119l@iespoblenou.org>
2016-04-26 17:09:21 +0200

    Funcionalitat login funciona correctament. (firebase ref és de proba ).

--- app/src/main/java/com/example/sandra/gymapp/LoginActivity.java
+++ app/src/main/java/com/example/sandra/gymapp/LoginActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -21,6 +22,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -31,6 +33,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sandra.gymapp.firebasefile.User;
 import com.firebase.client.AuthData;
@@ -76,6 +79,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         setContentView(R.layout.activity_login);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        Firebase.setAndroidContext(getBaseContext());
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
@@ -90,11 +94,34 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        final Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+
+
+                Firebase ref = new Firebase("https://fiery-inferno-9835.firebaseio.com/");
+
+
+                ref.authWithPassword(mEmailView.getText().toString(), mPasswordView.getText().toString(), new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
+                        Intent i = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        mEmailView.setText("");
+                        mPasswordView.setText("");
+                        Toast.makeText(getBaseContext(), "Autentificació fallida", Toast.LENGTH_LONG).show();
+
+
+                    }
+                });
+
+
             }
         });
 
