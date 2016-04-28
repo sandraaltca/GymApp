7ccf33f5c89feee83d1e2a2cbee34cb0fef09a97
47419119l <47419119l@iespoblenou.org>
2016-04-28 18:40:12 +0200

    Creada classe java infoGym

---
+++ app/src/main/java/com/example/sandra/gymapp/classesjava/InfoGym.java
@@ -0,0 +1,70 @@
+package com.example.sandra.gymapp.classesjava;
+
+/**
+ * Created by 47419119l on 28/04/16.
+ */
+public class InfoGym {
+    String nombreGym;
+    String direccionGym;
+    int telefonoGym;
+    String correoElectronicoGym;
+    long latitudGym;
+    long longitudGym;
+    String horarioGym[];
+
+    public String getNombreGym() {
+        return nombreGym;
+    }
+
+    public void setNombreGym(String nombreGym) {
+        this.nombreGym = nombreGym;
+    }
+
+    public String getDireccionGym() {
+        return direccionGym;
+    }
+
+    public void setDireccionGym(String direccionGym) {
+        this.direccionGym = direccionGym;
+    }
+
+    public int getTelefonoGym() {
+        return telefonoGym;
+    }
+
+    public void setTelefonoGym(int telefonoGym) {
+        this.telefonoGym = telefonoGym;
+    }
+
+    public String getCorreoElectronicoGym() {
+        return correoElectronicoGym;
+    }
+
+    public void setCorreoElectronicoGym(String correoElectronicoGym) {
+        this.correoElectronicoGym = correoElectronicoGym;
+    }
+
+    public long getLatitudGym() {
+        return latitudGym;
+    }
+
+    public void setLatitudGym(long latitudGym) {
+        this.latitudGym = latitudGym;
+    }
+
+    public long getLongitudGym() {
+        return longitudGym;
+    }
+
+    public void setLongitudGym(long longitudGym) {
+        this.longitudGym = longitudGym;
+    }
+
+    public String[] getHorarioGym() {
+        return horarioGym;
+    }
+
+    public void setHorarioGym(String[] horarioGym) {
+        this.horarioGym = horarioGym;
+    }
+}
