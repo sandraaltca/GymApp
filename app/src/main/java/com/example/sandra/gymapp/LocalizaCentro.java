b888d297f0a92c2310d85b090d8f1d9b7653e937
47419119l <47419119l@iespoblenou.org>
2016-04-29 15:50:50 +0200

    Configuració Llistat localiza centro funciona correctament

--- app/src/main/java/com/example/sandra/gymapp/LocalizaCentro.java
+++ app/src/main/java/com/example/sandra/gymapp/LocalizaCentro.java
@@ -1,5 +1,6 @@
 package com.example.sandra.gymapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,11 +8,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
+import com.example.sandra.gymapp.classesjava.InfoGym;
+import com.firebase.ui.FirebaseListAdapter;
 
 
 public class LocalizaCentro extends Fragment {
-
+    private Firebase infoGymRef;
+    private Firebase ref;
+    private ListView listCentre;
 
     public LocalizaCentro() {
         // Required empty public constructor
@@ -22,10 +33,56 @@ public class LocalizaCentro extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_localiza_centro, container, false);
-    }
+        View rootView = inflater.inflate(R.layout.fragment_localiza_centro, container, false);
 
+        /**
+         * Contecem amb firebase.
+         */
+        Firebase.setAndroidContext(getContext());
+        ref = new Firebase("https://testgimmapp.firebaseio.com/");
+        infoGymRef = ref.child("InfoGym");
+        /**
+         * Objectes layout.
+         */
+
+        listCentre = (ListView)rootView.findViewById(R.id.listCentre);
+
+        /**
+         * Configuració
+         */
+
+        configLlistatGimnas();
+        return rootView;
+    }
+    /**
+     * Metode per configurar el llistat de gimnasos.
+     */
+    private void configLlistatGimnas(){
+
+        FirebaseListAdapter adapter = new FirebaseListAdapter<InfoGym>(getActivity(), InfoGym.class, R.layout.list_localiza_centro, infoGymRef) {
+            @Override
+            protected void populateView(View v, InfoGym info, int position) {
+                TextView nom = (TextView) v.findViewById(R.id.nomCentre);
+                TextView direccio = (TextView) v.findViewById(R.id.ubicacioCentre);
+                TextView email = (TextView) v.findViewById(R.id.emailCentre);
+                TextView telefon = (TextView) v.findViewById(R.id.telfCentre);
+                TextView semana = (TextView) v.findViewById(R.id.horariCentre);
+                TextView horari2 = (TextView)v.findViewById(R.id.horari2);
+                TextView horari3 = (TextView)v.findViewById(R.id.horari3);
+
+
+                nom.setText(info.getNombreGym());
+                direccio.setText(info.getDireccionGym());
+                email.setText(info.getCorreoElectronicoGym());
+                telefon.setText(String.valueOf(info.getTelefonoGym()));
+                semana.setText("Lunes- viernes : " + info.getHorarioGym()[0]);
+                horari2.setText("Sabados : " + info.getHorarioGym()[1]);
+                horari3.setText("Domingos y festivos : " + info.getHorarioGym()[2]);
+
+            }
+        };
+        listCentre.setAdapter(adapter);
+    }
 
     /**
      * This interface must be implemented by activities that contain this
