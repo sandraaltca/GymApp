33277ff17cb16f7a55230fcd9ab0f48abc90080e
47419119l <47419119l@iespoblenou.org>
2016-04-29 16:36:34 +0200

    Creat layout interficie chat Classe java chat creada

---
+++ app/src/main/java/com/example/sandra/gymapp/classesjava/Chat.java
@@ -0,0 +1,28 @@
+package com.example.sandra.gymapp.classesjava;
+
+/**
+ * Created by 47419119l on 29/04/16.
+ */
+public class Chat {
+
+    private String message;
+    private String author;
+
+    // Required default constructor for Firebase object mapping
+    @SuppressWarnings("unused")
+    private Chat() {
+    }
+
+    Chat(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
